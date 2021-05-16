2a3342ce25f2202092d00dbd19f295cc24f51fc9
luisyerbes20 <al1609127@upy.edu.mx>
2021-05-16 09:46:42 -0500

    edit Identify view

--- frontend/src/views/oai/OaiIdentify.vue
+++ frontend/src/views/oai/OaiIdentify.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>HI!</div>
+<section>
+<div id="main" style="display: flex;">
+<div id="variables_name">
+	<article>
+	    <h3>Name Repository:</h3>
+	    <br />
+	    <h3>Base URL:</h3>
+	    <br />
+	    <h3>Prococol Version:</h3>
+	    <br />
+	    <h3> E-Mail Contact:</h3>
+	    <br />
+	    <h3> Earliest Datestamp:</h3>
+	    <br />
+	    <h3> Deleted Record:</h3>
+	    <br />
+	    <h3>Granularity:</h3>
+	</article>
+</div>
+<div id="data_identify">
+	<article>
+	    <h3 class="data"> Cenoteando</h3>
+	    <br />
+	    <h3 class="data"> https://cenoteando.org/oai/request</h3>
+	    <br />
+	    <h3 class="data"> 2.0</h3>
+	    <br />
+	    <h3 class="data"> cenoteando@gmail.com</h3>
+	    <br />
+	    <h3 class="data"> 2021-01-01T00:00:00z</h3>
+	    <br />
+	    <h3 class="data"> no </h3>
+	    <br />
+	    <h3 class="data"> YYYY-MM-DDThh:mm:ssZ</h3>
+	</article>
+</div>
+</div>
+</section>
 </template>
 
 <script>
@@ -10,4 +47,4 @@ export default Vue.extend({
 });
 </script>
 
-<style scoped></style>
+<style> @import './style.css';</style>
