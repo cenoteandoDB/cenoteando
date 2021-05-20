8b4ed65fc027120de26bbcf0ab7cd3d6341f68cc
luisyerbes20 <al1609127@upy.edu.mx>
2021-05-20 17:24:30 -0500

    style getrecord

--- frontend/src/views/oai/OaiGetRecord.vue
+++ frontend/src/views/oai/OaiGetRecord.vue
@@ -1,17 +1,38 @@
 <template>
     <v-container class="mt-10">
+	<div id="Tittle">
         <v-row class="mb-10 mx-1">
-            <h2>Get Record</h2>
+            <h2>GET RECORD</h2>
         </v-row>
+	</div>
+	<br />
         <!-- TODO: Create and link backend endpoints for downloading JSON and CSV -->
         <!-- TODO: Layout -->
+	<div id="search">
         <span>Identifier:</span>
+	<div id="identifier">
         <v-text-field v-model="identifier"></v-text-field>
+	</div>
+	</div>
+	<br />
+	<div id="bottoms" style="display:flex">
+	<div id="bottom">
         <v-btn v-on:click="getRecordXml">Get Record</v-btn>
+	</div>
+	<div id="download">
+	<v-btn class="downBO" v-on:click="download">Download metadata</v-btn>
+	</div>
+	</div>
+	<br />
+	<div id="xml_section">
+	<span>Metadata:</span>
+	<div id="xml">
         <xml-viewer :xml="xml" />
+	</div>
+	</div>
     </v-container>
 </template>
-
+<style> @import './style.css'; </style>
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import RemoteServices from '@/services/RemoteServices';
@@ -21,7 +42,7 @@ import XmlViewer from 'vue-xml-viewer';
 export default class OaiListRecords extends Vue {
     xml = '';
     identifier = 'oai:cenoteando.org:Cenotes/1';
-
+    down= 'dowload metadata';
     async created(): Promise<void> {
         await this.getRecordXml();
     }
@@ -35,5 +56,10 @@ export default class OaiListRecords extends Vue {
         }
         await this.$store.dispatch('clearLoading');
     }
+	
+    async download(): Promise<void> {
+	const url = './metadate_cenote.txt';
+	window.open(url);
+}
 }
 </script>
