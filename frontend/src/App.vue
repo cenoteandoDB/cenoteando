eaea9a690ec6c8847c52f98b2efdcff7b6c1ec6f
luisyerbes20 <al1609127@upy.edu.mx>
2021-05-25 11:47:35 -0500

    editing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,18 +5,21 @@
             <error-message />
             <loading />
             <router-view />
-        </v-main>
+	
+	<app-footer></app-footer>
+	</v-main>
     </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
 import ErrorMessage from '@/components/ErrorMessage.vue';
 import Loading from '@/components/Loading.vue';
 
 @Component({
-    components: { NavBar, ErrorMessage, Loading },
+    components: { NavBar, ErrorMessage, Loading, 'app-footer': Footer },
 })
 export default class App extends Vue {}
 </script>
