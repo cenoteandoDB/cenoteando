27595458c0ec8621e8751b62a4d52a87ad6cc297
luisyerbes20 <al1609127@upy.edu.mx>
2021-05-28 20:07:35 -0500

    actualized App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,12 +13,12 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import NavBar from '@/components/NavBar.vue';
-import footer from '@/components/Footer.vue';
+import Footer from '@/components/Footer.vue';
 import ErrorMessage from '@/components/ErrorMessage.vue';
 import Loading from '@/components/Loading.vue';
 
 @Component({
-    components: { NavBar, ErrorMessage, Loading, 'app-footer': footer},
+    components: { NavBar, ErrorMessage, Loading, 'app-footer': Footer},
 })
 export default class App extends Vue {}
 </script
