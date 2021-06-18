2170557dc022214541814af86ba4bb069bbab637
retroazure <tomascosta005@outlook.com>
2021-06-18 12:37:59 +0100

    navbar dropdown done(mostly?), just playing around with the map popup

--- frontend/src/components/map/LeafletMap.vue
+++ frontend/src/components/map/LeafletMap.vue
@@ -16,7 +16,19 @@
             >
                 <!-- TODO: :icon="marker_icon" -->
                 <!-- TODO: Add content + "more info." link to popup -->
-                <l-popup :content="cenote.properties.name"></l-popup>
+                <l-popup>
+                    <label>Name:</label>
+                    <h4>{{ cenote.properties.name }}</h4>
+
+                    <label>Type:</label>
+                    <h4>{{ cenote.properties.type }}</h4>
+
+                    <label>Touristic:</label>
+                    <h4>{{ cenote.properties.touristic }}</h4>
+
+                    <label>State:</label>
+                    <h4>{{ cenote.properties.state }}</h4>
+                </l-popup>
             </l-circle-marker>
         </l-marker-cluster>
     </l-map>
