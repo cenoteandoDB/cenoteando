6ee2f846b8c960f41eb9a52e6a47b3552145eb5f
retroazure <tomascosta005@outlook.com>
2021-06-23 18:42:59 +0100

    Tabs almost ready to have content in them, there's only an eslint error in the console

--- frontend/src/views/Cenote.vue
+++ frontend/src/views/Cenote.vue
@@ -63,13 +63,20 @@
             </v-col>
             <v-col cols="12" md="6">
                 <v-sheet elevation="2" style="min-height: 500px">
-                    <v-tabs>
-                        <v-tab>General</v-tab>
-                        <v-tab>Calculated Variables</v-tab>
-                        <v-tab>Social</v-tab>
-                        <v-tab>Thematic Data</v-tab>
-                        <v-tab>Sources</v-tab>
+                    <v-tabs v-model="tab" align-with-title>
+                        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+                        <v-tab v-for="item in items" :key="item">
+                            {{ item }}
+                        </v-tab>
                     </v-tabs>
+                    <v-tabs-items v-model="tab">
+                        <v-tab-item v-for="item in items" :key="item">
+                            <v-card flat>
+                                <v-card-text v-text="text"></v-card-text>
+                            </v-card>
+                        </v-tab-item>
+                    </v-tabs-items>
                 </v-sheet>
             </v-col>
         </v-row>
@@ -96,6 +103,19 @@ import MapMarker from '@/components/map/MapMarker.vue';
 export default class Cenote extends Vue {
     marker: L.CircleMarker | null = null;
     cenote: CenoteDTO | null = null;
+    data() {
+        return {
+            tab: null,
+            items: [
+                'General',
+                'Calculated Variables',
+                'Social',
+                'Thematic Data',
+                'Sources',
+            ],
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        };
+    }
     zoom = 12;
     url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     attribution =
