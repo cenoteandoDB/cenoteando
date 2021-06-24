603340304756f7e1d7199c13071265f19f6bc04c
retroazure <tomascosta005@outlook.com>
2021-06-24 11:05:55 +0100

    component CenoteDetails with tabs and their content, still in progress in terms of having the real content in the tabs

---
+++ frontend/src/components/CenoteDetails.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-sheet elevation="2" style="min-height: 500px">
+        <v-tabs fixed-tabs v-model="tab">
+            <v-tabs-slider color="amber darken-3"></v-tabs-slider>
+            <v-tab
+                v-for="(item, index) in items"
+                :class="{ active: currentTab === index }"
+                @click="currentTab = index"
+                :key="item"
+            >
+                {{ item }}
+            </v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+            <v-card flat>
+                <div v-show="currentTab === 0">
+                    <v-card-text>General tab Content </v-card-text>
+                </div>
+                <div v-show="currentTab === 1">
+                    <v-card-text>Tab1 content</v-card-text>
+                </div>
+                <div v-show="currentTab === 2">
+                    <v-card-text>Tab2 content</v-card-text>
+                </div>
+                <div v-show="currentTab === 3">
+                    <v-card-text>Tab3 content</v-card-text>
+                </div>
+                <div v-show="currentTab === 4">
+                    <v-card-text>Tab4 content</v-card-text>
+                </div>
+            </v-card>
+        </v-tabs-items>
+    </v-sheet>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class CenoteDetails extends Vue {
+    currentTab = 0;
+    tab = null;
+    items = [
+        'General',
+        'Calculated Variables',
+        'Social',
+        'Thematic Data',
+        'Sources',
+    ];
+}
+</script>
+
+<style scoped lang="scss"></style>
--- frontend/src/views/Cenote.vue
+++ frontend/src/views/Cenote.vue
@@ -62,38 +62,7 @@
                 </v-row>
             </v-col>
             <v-col cols="12" md="6">
-                <v-sheet elevation="2" style="min-height: 500px">
-                    <v-tabs fixed-tabs v-model="tab">
-                        <v-tabs-slider color="amber darken-3"></v-tabs-slider>
-                        <v-tab
-                            v-for="(item, index) in items"
-                            :class="{ active: currentTab === index }"
-                            @click="currentTab = index"
-                            :key="item"
-                        >
-                            {{ item }}
-                        </v-tab>
-                    </v-tabs>
-                    <v-tabs-items v-model="tab">
-                        <v-card flat>
-                            <div v-show="currentTab === 0">
-                                <v-card-text>Tab0 content</v-card-text>
-                            </div>
-                            <div v-show="currentTab === 1">
-                                <v-card-text>Tab1 content</v-card-text>
-                            </div>
-                            <div v-show="currentTab === 2">
-                                <v-card-text>Tab2 content</v-card-text>
-                            </div>
-                            <div v-show="currentTab === 3">
-                                <v-card-text>Tab3 content</v-card-text>
-                            </div>
-                            <div v-show="currentTab === 4">
-                                <v-card-text>Tab4 content</v-card-text>
-                            </div>
-                        </v-card>
-                    </v-tabs-items>
-                </v-sheet>
+                <cenote-details />
             </v-col>
         </v-row>
     </v-container>
@@ -107,6 +76,7 @@ import { LMap, LTileLayer, LCircleMarker } from 'vue2-leaflet';
 import RemoteServices from '@/services/RemoteServices';
 import CenoteDTO from '@/models/CenoteDTO';
 import MapMarker from '@/components/map/MapMarker.vue';
+import CenoteDetails from '@/components/CenoteDetails.vue';
 
 @Component({
     components: {
@@ -114,25 +84,13 @@ import MapMarker from '@/components/map/MapMarker.vue';
         LTileLayer,
         LCircleMarker,
         MapMarker,
+        CenoteDetails,
     },
 })
 export default class Cenote extends Vue {
     marker: L.CircleMarker | null = null;
     cenote: CenoteDTO | null = null;
-    data() {
-        return {
-            currentTab: 0,
-            tab: null,
-            items: [
-                'General',
-                'Calculated Variables',
-                'Social',
-                'Thematic Data',
-                'Sources',
-            ],
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        };
-    }
+
     zoom = 12;
     url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     attribution =
