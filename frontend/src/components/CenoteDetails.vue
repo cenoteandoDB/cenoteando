aed287a3aa93837f2bae948f726ddd279ce385fe
retroazure <tomascosta005@outlook.com>
2021-06-25 12:06:51 +0100

    working on tabs and icons, alongside their content

--- frontend/src/components/CenoteDetails.vue
+++ frontend/src/components/CenoteDetails.vue
@@ -3,18 +3,49 @@
         <v-tabs fixed-tabs v-model="tab">
             <v-tabs-slider color="amber darken-3"></v-tabs-slider>
             <v-tab
-                v-for="(item, index) in items"
+                v-for="(tab, index) in tabs"
                 :class="{ active: currentTab === index }"
                 @click="currentTab = index"
-                :key="item"
+                :key="tab"
             >
-                {{ item }}
+                {{ tab }}
             </v-tab>
         </v-tabs>
         <v-tabs-items v-model="tab">
             <v-card flat>
                 <div v-show="currentTab === 0">
-                    <v-card-text>General tab Content </v-card-text>
+                    <v-container>
+                        <v-row class="ml-10 mt-5" no-gutters>
+                            <v-col>
+                                <v-icon class="ml-10 mr-10 mt-5"
+                                    >mdi-source-branch</v-icon
+                                >
+                            </v-col>
+                            <v-col order="2">
+                                <h3 class="ml-10 mr-10 mt-5">Kayak Rental</h3>
+                            </v-col>
+                            <v-col order="3">
+                                <v-icon class="mt-5 ml-15" color="green"
+                                    >mdi-check
+                                </v-icon>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <v-container>
+                        <v-row no-gutters class="ml-10 mt-5">
+                            <v-col>
+                                <v-icon class="ml-10 mt-5">mdi-swim</v-icon>
+                            </v-col>
+                            <v-col order="2">
+                                <h3 class="ml-5 mr-5 mt-5">Swimming</h3>
+                            </v-col>
+                            <v-col order="3">
+                                <v-icon class="mt-5 ml-15" color="green"
+                                    >mdi-close
+                                </v-icon>
+                            </v-col>
+                        </v-row>
+                    </v-container>
                 </div>
                 <div v-show="currentTab === 1">
                     <v-card-text>Tab1 content</v-card-text>
@@ -39,7 +70,7 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class CenoteDetails extends Vue {
     currentTab = 0;
     tab = null;
-    items = [
+    tabs = [
         'General',
         'Calculated Variables',
         'Social',
