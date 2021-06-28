2b27506350aac557028b8560bb9af1025593f203
retroazure <tomascosta005@outlook.com>
2021-06-28 09:25:17 +0100

    Removed the icons from the other tabs, because they didn't make sense

--- frontend/src/components/CenoteDetails.vue
+++ frontend/src/components/CenoteDetails.vue
@@ -14,50 +14,27 @@
         <v-tabs-items v-model="tab">
             <v-card flat>
                 <div v-show="currentTab === 0">
-                    <v-container>
-                        <v-row class="ml-10 mt-5" no-gutters>
-                            <v-col>
-                                <v-icon class="ml-10 mr-10 mt-5"
-                                    >mdi-source-branch</v-icon
-                                >
-                            </v-col>
-                            <v-col order="2">
-                                <h3 class="ml-10 mr-10 mt-5">Kayak Rental</h3>
-                            </v-col>
-                            <v-col order="3">
-                                <v-icon class="mt-5 ml-15" color="green"
-                                    >mdi-check
-                                </v-icon>
-                            </v-col>
-                        </v-row>
-                    </v-container>
-                    <v-container>
-                        <v-row no-gutters class="ml-10 mt-5">
-                            <v-col>
-                                <v-icon class="ml-10 mt-5">mdi-swim</v-icon>
-                            </v-col>
-                            <v-col order="2">
-                                <h3 class="ml-5 mr-5 mt-5">Swimming</h3>
-                            </v-col>
-                            <v-col order="3">
-                                <v-icon class="mt-5 ml-15" color="green"
-                                    >mdi-close
-                                </v-icon>
-                            </v-col>
-                        </v-row>
+                    <v-container class="d-flex justify-center mb-6">
+                        <v-icon x-large color="green" class="ma-5"
+                            >mdi-swim</v-icon
+                        >
+
+                        <v-icon x-large color="red" class="mr-15"
+                            >mdi-swim</v-icon
+                        >
                     </v-container>
                 </div>
                 <div v-show="currentTab === 1">
-                    <v-card-text>Tab1 content</v-card-text>
+                    <v-card-text>Content</v-card-text>
                 </div>
                 <div v-show="currentTab === 2">
-                    <v-card-text>Tab2 content</v-card-text>
+                    <v-card-text>Content</v-card-text>
                 </div>
                 <div v-show="currentTab === 3">
-                    <v-card-text>Tab3 content</v-card-text>
+                    <v-card-text>Content</v-card-text>
                 </div>
                 <div v-show="currentTab === 4">
-                    <v-card-text>Tab4 content</v-card-text>
+                    <v-card-text>Content</v-card-text>
                 </div>
             </v-card>
         </v-tabs-items>
