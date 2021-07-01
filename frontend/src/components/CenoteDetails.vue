42c854acf20499529cd9337d69b7cacb5ca48bad
retroazure <tomascosta005@outlook.com>
2021-07-01 11:01:38 +0100

    Added a tooltip as a sample to indicate what each icon means

--- frontend/src/components/CenoteDetails.vue
+++ frontend/src/components/CenoteDetails.vue
@@ -17,10 +17,20 @@
                     <v-row class="pt-10">
                         <v-col class="d-flex justify-center">
                             <v-spacer></v-spacer>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon
+                                        x-large
+                                        color="green"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                        dense
+                                        >mdi-swim</v-icon
+                                    >
+                                </template>
+                                <span>Swimming available</span>
+                            </v-tooltip>
 
-                            <v-icon x-large color="green" dense
-                                >mdi-swim</v-icon
-                            >
                             <v-spacer></v-spacer>
                             <v-icon x-large color="red" dense>mdi-tent</v-icon>
                             <v-spacer></v-spacer>
@@ -63,56 +73,59 @@
                 </div>
 
                 <div v-show="currentTab === 1">
-                    <v-row class="pt-10 pl-10 pr-10">
-                        <v-col class="d-flex justify-center m-10">
+                    <v-row class="pt-10">
+                        <v-col class="d-flex justify-center">
+                            <v-spacer></v-spacer>
+                            <v-tooltip bottom>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-icon
+                                        x-large
+                                        color="green"
+                                        v-bind="attrs"
+                                        v-on="on"
+                                        dense
+                                        >mdi-swim</v-icon
+                                    >
+                                </template>
+                                <span>Swimming available</span>
+                            </v-tooltip>
+
+                            <v-spacer></v-spacer>
+                            <v-icon x-large color="red" dense>mdi-tent</v-icon>
                             <v-spacer></v-spacer>
 
-                            <v-avatar
-                                class="p-10"
-                                color="primary"
-                                size="40"
-                            ></v-avatar>
-
-                            <v-card-text>User Name</v-card-text>
+                            <v-icon x-large color="green" dense
+                                >mdi-silverware-fork-knife
+                            </v-icon>
                             <v-spacer></v-spacer>
-                            <v-rating
-                                :value="4.5"
-                                color="amber"
-                                dense
-                                half-increments
-                                readonly
-                                size="14"
-                            ></v-rating>
 
+                            <v-icon x-large color="green" dense
+                                >mdi-shower-head
+                            </v-icon>
                             <v-spacer></v-spacer>
                         </v-col>
                     </v-row>
-                    <v-row class="pl-10 m-10">
+                    <v-row>
                         <v-col class="d-flex justify-center mx-auto">
                             <v-spacer></v-spacer>
 
-                            <v-card-text size="x-larger"
-                                >At vero eos et accusamus et iusto odio
-                                dignissimos ducimus qui blanditiis praesentium
-                                voluptatum deleniti atque corrupti quos dolores
-                                et quas molestias excepturi sint occaecati
-                                cupiditate non provident, similique sunt in
-                                culpa qui officia deserunt mollitia animi, id
-                                est laborum et dolorum fuga. Et harum quidem
-                                rerum facilis est et expedita distinctio. Nam
-                                libero tempore, cum soluta nobis est eligendi
-                                optio cumque nihil impedit quo minus id quod
-                                maxime placeat facere possimus, omnis voluptas
-                                assumenda est, omnis dolor repellendus.
-                                Temporibus autem quibusdam et aut officiis
-                                debitis aut rerum necessitatibus saepe eveniet
-                                ut et voluptates repudiandae sint et molestiae
-                                non recusandae. Itaque earum rerum hic tenetur a
-                                sapiente delectus, ut aut reiciendis
-                                voluptatibus maiores alias consequatur aut
-                                perferendis doloribus asperiores repellat.
-                            </v-card-text>
+                            <v-icon x-large color="green" dense
+                                >mdi-food
+                            </v-icon>
+                            <v-spacer></v-spacer>
+                            <v-icon x-large color="red" dense
+                                >mdi-hanger
+                            </v-icon>
+                            <v-spacer></v-spacer>
 
+                            <v-icon x-large color="green" dense
+                                >mdi-paper-roll-outline
+                            </v-icon>
+                            <v-spacer></v-spacer>
+
+                            <v-icon x-large color="green" dense
+                                >mdi-horse-human
+                            </v-icon>
                             <v-spacer></v-spacer>
                         </v-col>
                     </v-row>
