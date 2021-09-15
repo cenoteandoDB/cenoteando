580c9b67c95658af8041f6c064a400ebabdb1e7e
retroazure <retroazure@DESKTOP-2AHB5TE.localdomain>
2021-09-15 12:13:02 +0100

    First test on cypress set up

--- frontend/tests/e2e/specs/test.js
+++ frontend/tests/e2e/specs/test.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
     it('Visits the app root url', () => {
-        cy.visit('/');
-        cy.contains('h1', 'Welcome to Your Vue.js + TypeScript App');
+        cy.visit('http://localhost:8080/');
+        cy.contains('h2', 'Repositorio institucional de cenotes');
     });
 });
