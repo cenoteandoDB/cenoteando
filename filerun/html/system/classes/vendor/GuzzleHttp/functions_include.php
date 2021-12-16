8649dc6c8c9eb8d86854073ce04d8d894772c735
Diogo Repas <diogo.repas@gmail.com>
2021-12-16 20:33:00 +0000

    Remove filerun and minor fixes

--- filerun/html/system/classes/vendor/GuzzleHttp/functions_include.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Don't redefine the functions if included multiple times.
-if (!function_exists('GuzzleHttp\uri_template')) {
-	require __DIR__ . '/functions.php';
-	require(__DIR__ .'/Psr7/functions.php');
-	require(__DIR__ .'/Promise/functions.php');
-}
--- filerun/html/wl/js/gallery.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$('.swipebox').swipebox();
-});
