4b5702b7acb6cb242b6bdf58fde0df9e8351f859
DRepas <diogo.repas@gmail.com>
2022-02-28 02:32:29 +0000

    Minor fixes

--- backend/src/test/java/org/cenoteando/CenoteandoBackendApplicationTests.java
+++ backend/src/test/java/org/cenoteando/CenoteandoBackendApplicationTests.java
@@ -1,9 +1,7 @@
 package org.cenoteando;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CenoteandoBackendApplicationTests {
 
 	@Test
