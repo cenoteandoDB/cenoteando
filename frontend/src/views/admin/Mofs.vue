8975952fd50aa012b785670d5bf07e3f6b39a62a
retroazure <tomhellyeah@gmail.com>
2022-04-10 14:52:27 +0100

    Made some changes to mofs admin panel layout, adding buttons and createmof dialog, also added the count

--- frontend/src/views/admin/Mofs.vue
+++ frontend/src/views/admin/Mofs.vue
@@ -4,13 +4,15 @@
             <v-select
                 :items="filteredCenotes()"
                 v-model="selectedCenote"
-                v-on:change="()=> {
-                    this.selectedCenoteFlag = true;
-                    
-                    if(this.selectedThemeFlag === true){
-                        getMofData();
+                v-on:change="
+                    () => {
+                        this.selectedCenoteFlag = true;
+
+                        if (this.selectedThemeFlag === true) {
+                            getMofData();
+                        }
                     }
-                    }"
+                "
                 label="Select Cenote"
                 solo
             >
@@ -18,12 +20,14 @@
             <v-select
                 :items="themes"
                 v-model="selectedTheme"
-                v-on:change="()=> {
-                    this.selectedThemeFlag = true;
-                    if(this.selectedCenoteFlag === true) {
-                        getMofData();
+                v-on:change="
+                    () => {
+                        this.selectedThemeFlag = true;
+                        if (this.selectedCenoteFlag === true) {
+                            getMofData();
+                        }
                     }
-                    }"
+                "
                 label="Select Theme"
                 solo
             ></v-select>
@@ -32,16 +36,26 @@
         <v-data-table
             v-if="selectedCenoteFlag === true && selectedThemeFlag === true"
             :headers="headers"
-            :items="mofs.map((m) => m.variable)"
+            :items="
+                mofs.map((m) => {
+                    m.variable['count'] = m.values.reduce(
+                        (a, obj) => a + Object.keys(obj).length,
+                        0,
+                    );
+                    return m.variable;
+                })
+            "
             :items-per-page="15"
             :search="search"
             class="elevation-1"
-            v-on:change="()=> {
+            v-on:change="
+                () => {
                     this.selectedThemeFlag = true;
-                    }"
+                }
+            "
         >
             <template v-slot:top>
-                <v-card-title>
+                <v-card-title class="flex-column">
                     <v-text-field
                         v-model="search"
                         append-icon="mdi-magnify"
@@ -50,8 +64,65 @@
                     />
                     <v-spacer />
                     <v-container class="d-flex flex-row justify-center">
+                        <v-dialog max-width="600px">
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    v-on="on"
+                                    v-bind="attrs"
+                                    data-cy="uploadButton"
+                                    ><v-icon color="primary"
+                                        >mdi-upload</v-icon
+                                    ></v-btn
+                                >
+                            </template>
+                            <v-card class="pt-5 mt-5 justify-center">
+                                <v-card-title>
+                                    <span class="text-h5">Upload Mofs</span>
+                                </v-card-title>
+                                <v-card-text>
+                                    <v-form>
+                                        <v-file-input
+                                            @change="selectFiles"
+                                            multiple
+                                            counter
+                                            show-size
+                                            chips
+                                            accept=".csv"
+                                        />
+                                    </v-form>
+                                    <!-- TODO: Add progressbar
+                                    <v-progress-linear
+                                        :value="this.uploadProgress"
+                                    ></v-progress-linear>
+                                    -->
+                                    <v-btn @click="upload">Upload</v-btn>
+                                </v-card-text>
+                            </v-card>
+                        </v-dialog>
+
+                        <v-btn @click="download" data-cy="downloadButton"
+                            ><v-icon color="primary"
+                                >mdi-download</v-icon
+                            ></v-btn
+                        >
                     </v-container>
                 </v-card-title>
+            </template>
+
+            <template v-slot:[`item.action`]="{ item }">
+                <edit-mofs-dialog :mofs="item" @onSave="updateVariable(item)">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                            class="action-button"
+                            :data-cy="'editVariable_' + item.id"
+                            v-on="on"
+                            v-bind="attrs"
+                            color="green"
+                            >mdi-pencil</v-icon
+                        >
+                    </template>
+                </edit-mofs-dialog>
+                <delete-dialog @onConfirm="deleteCenote(item.cenote)" />
             </template>
         </v-data-table>
     </v-card>
@@ -64,7 +135,7 @@
 
 <script lang="ts">
 import DeleteDialog from '@/components/admin/DeleteDialog.vue';
-import EditReferenceDialog from '@/components/admin/EditReferenceDialog.vue';
+import EditMofsDialog from '@/components/admin/EditMofsDialog.vue';
 import CenoteDTO from '@/models/CenoteDTO';
 import VariableDTO from '@/models/VariableDTO';
 import FileService from '@/services/FileService';
@@ -81,7 +152,7 @@ interface MofsData {
 
 @Component({
     components: {
-        EditReferenceDialog,
+        EditMofsDialog,
         DeleteDialog,
     },
 })
@@ -90,15 +161,10 @@ export default class Mofs extends Vue {
     uploadProgress = 0;
     headers = [
         { text: 'Name', value: 'name' },
-        { text: 'Origin', value: 'origin' },
-        { text: 'Theme', value: 'theme' },
-        { text: 'description', value: 'description' },
+        { text: 'Description', value: 'description' },
         { text: 'Access Level', value: 'accessLevel' },
         { text: 'Data Type', value: 'type' },
-        { text: 'Timeseries', value: 'timeseries' },
-        { text: 'Multiple', value: 'multiple' },
-        { text: 'Enum Values', value: 'enumValues' },
-        { text: 'Mofs values', value: 'values.timestamp'},
+        { text: 'Count', value: 'count' },
         { text: 'Actions', value: 'action' },
     ];
 
@@ -124,6 +190,7 @@ export default class Mofs extends Vue {
     item = [];
     search = '';
     newVariable = new VariableDTO();
+    newMofs = new VariableWithValuesDTO();
     variables: VariableDTO[] = [];
     cenotes: CenoteDTO[] = [];
     mofs: VariableWithValuesDTO[] = [];
@@ -187,11 +254,11 @@ export default class Mofs extends Vue {
         await this.$store.dispatch('loading');
 
         try {
-            if(this.selectedCenote && this.selectedTheme){
-                
-            this.mofs = await RemoteServices.getData(this.cenoteDisplayToId(this.selectedCenote), this.selectedTheme);
-            console.log(this.mofs.map((m)=> { return m.variable }));
-            
+            if (this.selectedCenote && this.selectedTheme) {
+                this.mofs = await RemoteServices.getData(
+                    this.cenoteDisplayToId(this.selectedCenote),
+                    this.selectedTheme,
+                );
             }
         } catch (error) {
             await this.$store.dispatch('error', error);
